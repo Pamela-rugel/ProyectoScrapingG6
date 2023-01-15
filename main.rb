aa9d9658364f69d60b22e1529247dab811cde095
alanbarco <alaxbarc@espol.edu.ec>
2023-01-15 15:24:03 -0500

    Scrapping Alan Barco

--- main.rb
+++ main.rb
@@ -2,11 +2,13 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 require './plataformas/Freelancer'
-
+require './plataformas/GrabJobs'
 #------------------------------------------- URLS A SCRAPEAR -------------------------------------------
 urlFreelancer = 'https://www.freelancer.ec/job-search/developer/' 
-
+urlGrabJobs = 'https://grabjobs.co/ecuador/jobs?q=desarrollador+developer+software+web+movil+Java+JavaScript+Python+C+C+R+PHP+Go+Ruby+Objective+C+C&p=' 
 # ---------------------------------------------- SCRAPING ---------------------------------------------
-puts "Freelancers"
-Freelancer.new.extraerData(urlFreelancer)
+#puts "Freelancers"
+#Freelancer.new.extraerData(urlFreelancer)
 
+puts "GrabJobs"
+GrabJobs.new.extraerData(urlGrabJobs)
