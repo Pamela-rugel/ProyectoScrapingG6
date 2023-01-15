f9be46dc6869cd53fb65d089efd0309d1f3b5c58
José Jaramillo <josjjara@espol.edu.ec>
2023-01-15 17:05:38 -0500

    Jora scrapper

--- main.rb
+++ main.rb
@@ -3,12 +3,18 @@ require 'nokogiri'
 require 'csv'
 require './plataformas/Freelancer'
 require './plataformas/GrabJobs'
+require './plataformas/Jora'
 #------------------------------------------- URLS A SCRAPEAR -------------------------------------------
 urlFreelancer = 'https://www.freelancer.ec/job-search/developer/' 
 urlGrabJobs = 'https://grabjobs.co/ecuador/jobs?q=desarrollador+developer+software+web+movil+Java+JavaScript+Python+C+C+R+PHP+Go+Ruby+Objective+C+C&p=' 
+urlJoraPt1 = 'https://ec.jora.com/j?l=Ecuador&p=' 
+urlJoraPt2 = '&q=Software+developer&sp=search&surl=0&tk=LY7GRVzLKKkhT_hHHCIz-K0XYrcCvI4SpH1jYTraQ'
 # ---------------------------------------------- SCRAPING ---------------------------------------------
 #puts "Freelancers"
 #Freelancer.new.extraerData(urlFreelancer)
 
-puts "GrabJobs"
-GrabJobs.new.extraerData(urlGrabJobs)
+# puts "GrabJobs"
+# GrabJobs.new.extraerData(urlGrabJobs)
+
+puts "Jora"
+Jora.new.extraerData(urlJoraPt1,urlJoraPt2)
---
+++ plataformas/Jora.rb
@@ -0,0 +1,25 @@
+class Jora
+  def extraerData(urlpt1, urlpt2)
+    CSV.open('Jora.csv', 'wb') do |csv|
+      csv << %w[num titulo_empleo descripcion_empleo etiquetas_empleo]
+      conf=0; pagina=1
+      while (pagina<7)
+        puts "Scrapeando la url #{urlpt1}#{pagina}#{urlpt2}"
+        link = "#{urlpt1}#{pagina}#{urlpt2}"
+        multitrabajosHTML = URI.open(link)
+        datos = multitrabajosHTML.read
+        parsed_content = Nokogiri::HTML(datos)
+        inf_container = parsed_content.css('.job-card').each do |oferta|          
+          titulo = oferta.css('.top-container').css('.job-title').css('.job-link').inner_text
+          descripcion = oferta.css('.job-abstract').inner_text
+          etiquetas = oferta.css('.info-container').css('.job-company').inner_text
+    
+          csv << [conf.to_s, titulo.to_s, descripcion.to_s, etiquetas.to_s]
+          conf+=1
+        end
+        pagina+=1
+      end
+      puts "Fin de Extracción, Jora ofertas laborales"
+    end
+  end
+end
